f986f2623442dabd20fd6c830b1f147367a9ea4c
Claus Vium <clausvium@gmail.com>
2019-02-24 13:46:01 +0100

    Convert tabs to spaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 const path = require('path');
 
 module.exports = {
-	entry: {
-		apiclient: './src/apiclient.js',
-		apiclientex: './src/apiclientex.js',
-		'appstorage-localstorage': './src/appstorage-localstorage.js',
-		'appstorage-memory': './src/appstorage-memory.js',
-		connectionmanager: './src/connectionmanager.js',
-		credentials: './src/credentials.js',
-		events: './src/events.js',
-		localassetmanager: './src/localassetmanager.js',
-		serverdiscovery: './src/serverdiscovery.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(dist|node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			}
-		]
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist'),
-		library: '[name]',
-		libraryTarget: 'umd',
-		libraryExport: 'default'
-	}
+    entry: {
+        apiclient: './src/apiclient.js',
+        apiclientex: './src/apiclientex.js',
+        'appstorage-localstorage': './src/appstorage-localstorage.js',
+        'appstorage-memory': './src/appstorage-memory.js',
+        connectionmanager: './src/connectionmanager.js',
+        credentials: './src/credentials.js',
+        events: './src/events.js',
+        localassetmanager: './src/localassetmanager.js',
+        serverdiscovery: './src/serverdiscovery.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(dist|node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        library: '[name]',
+        libraryTarget: 'umd',
+        libraryExport: 'default'
+    }
 };
