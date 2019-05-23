01e95fe91e35acc73700eda783c2b80ca31cb881
Bill Thornton <billt2006@gmail.com>
2019-05-23 00:30:56 -0400

    Add unified entry point

---
+++ src/sync/index.js
@@ -0,0 +1,19 @@
+import FileRepository from 'sync/filerepository.js';
+import ItemRepository from 'sync/itemrepository.js';
+import LocalSync from 'sync/localsync.js';
+import MediaSync from 'sync/mediasync.js';
+import MultiServerSync from 'sync/multiserversync.js';
+import ServerSync from 'sync/serversync.js';
+import TransferManager from 'sync/transfermanager.js';
+import UserActionRepository from 'sync/useractionrepository.js';
+
+export default {
+    FileRepository,
+    ItemRepository,
+    LocalSync,
+    MediaSync,
+    MultiServerSync,
+    ServerSync,
+    TransferManager,
+    UserActionRepository
+}
