621c88e0498228982aadf3322d0a6664f4d5ad5d
MrTimscampi <julien.machiels@protonmail.com>
2020-03-24 21:12:40 +0100

    Add first ApiClient tests

---
+++ tests/apiClient.test.js
@@ -0,0 +1,55 @@
+import apiClient from '../src/apiClient';
+
+describe("ApiClient", () => {
+    let client;
+
+    beforeEach(() => {
+        client = new apiClient("http://demo.jellyfin.org/stable", "Jellyfin Web", "10.5.0", "Firefox", "TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0OyBydjo3NC4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94Lzc0LjB8MTU4NDkwMTA5OTY3NQ11");
+    });
+
+    it('is instantiable', () => {
+        expect(client).toBeInstanceOf(apiClient);
+    });
+
+    it('has the expected constructor', () => {
+        expect(client._serverAddress).toBe("http://demo.jellyfin.org/stable");
+        expect(client._appName).toBe("Jellyfin Web");
+        expect(client._appVersion).toBe("10.5.0");
+        expect(client._deviceName).toBe("Firefox");
+        expect(client._deviceId).toBe("TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0OyBydjo3NC4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94Lzc0LjB8MTU4NDkwMTA5OTY3NQ11");
+    });
+
+    it('can get serverAddress', () => {
+        expect(client.serverAddress()).toBe("http://demo.jellyfin.org/stable");
+    });
+
+    it('can get appName', () => {
+        expect(client.appName()).toBe("Jellyfin Web");
+    });
+
+    it('can get appVersion', () => {
+        expect(client.appVersion()).toBe("10.5.0");
+    });
+
+    it('can get deviceName', () => {
+        expect(client.deviceName()).toBe("Firefox");
+    });
+
+    it('can get deviceId', () => {
+        expect(client.deviceId()).toBe("TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0OyBydjo3NC4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94Lzc0LjB8MTU4NDkwMTA5OTY3NQ11");
+    });
+
+    it('can throw error on setting an invalid server address', () => {
+        expect(() => {
+            client.serverAddress(Error)
+        }).toThrow(Error);
+    });
+
+    it("can change server address", () => {
+        expect(client.serverAddress("http://demo.jellyfin.org/nightly")).toBe("http://demo.jellyfin.org/nightly");
+    });
+
+    it("can get a URL", () => {
+        expect(client.getUrl("/System/Info/Public")).toBe("http://demo.jellyfin.org/stable/System/Info/Public")
+    })
+});
