eb09d137e55951a80fbc4b584b29c3c0549e5274
MrTimscampi <julien.machiels@protonmail.com>
2020-03-25 17:33:49 +0100

    Fix Jest issues with TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-
-  // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: [
-    "cobertura",
-  ],
+    clearMocks: true,
+    coverageDirectory: "coverage",
+    coverageReporters: [
+        "cobertura",
+    ],
+    moduleFileExtensions: ['ts', 'js'],
+    transform: {
+        '^.+\\.ts?$': 'ts-jest',
+        '^.+\\.js?$': 'ts-jest'
+    },
 };
