ad5c97b0861de44e5d6474ad1cffaecf46297de7
MrTimscampi <julien.machiels@protonmail.com>
2020-08-15 14:29:09 +0200

    Move to Rollup and bundle ESM

---
+++ src/main.js
@@ -0,0 +1,6 @@
+export { default as ApiClient } from './apiClient';
+export { default as ApiClientCore } from './apiClientCore';
+export { default as appStorage } from './appStorage';
+export { default as ConnectionManager } from './connectionManager';
+export { default as Credentials } from './credentials';
+export { default as events } from './events';
