4391b4d56c74714c386da2507ff52a6ea2cd268e
Bill Thornton <billt2006@gmail.com>
2021-09-27 00:59:35 -0400

    Fix babel target for jest

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
-module.exports = {
-    presets: [
-        [ '@babel/preset-env' ]
-    ]
+module.exports = api => {
+    const isTest = api.env('test');
+
+    return {
+        presets: [
+            isTest ? [
+                '@babel/preset-env',
+                // Jest needs to target node
+                { targets: { node: 'current' } }
+            ] : [
+                '@babel/preset-env'
+            ]
+        ]
+    };
 };
